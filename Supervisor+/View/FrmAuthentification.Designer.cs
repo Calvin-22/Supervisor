c4de0e4afabf7b4875b30fc04d2cbd28ed85deda
Calvin-22 <cal.editionpro@gmail.com>
2026-05-03 13:59:01 +0200

    Création du formulaire d'authentification. Ajout de l'icône (.ico), ajout d'un picturebox pour présenter l'application, tests du formulaire pour valider son fonctionnement. Masquage de la saisie du mot de passe.

--- Supervisor+/View/FrmAuthentification.Designer.cs
+++ Supervisor+/View/FrmAuthentification.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAuthentification));
             BtnSeConnecter = new SiticoneNetCoreUI.SiticoneButtonAdvanced();
             txtIdentifiant = new TextBox();
             txtMdp = new TextBox();
@@ -35,6 +36,8 @@
             lblmotdepasse = new Label();
             label3 = new Label();
             fakebutton = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // BtnSeConnecter
@@ -53,8 +56,8 @@
             BtnSeConnecter.BorderRadiusTopLeft = 35;
             BtnSeConnecter.BorderRadiusTopRight = 35;
             BtnSeConnecter.BorderThickness = 1;
-            BtnSeConnecter.ButtonColorEnd = Color.Green;
-            BtnSeConnecter.ButtonColorStart = Color.LimeGreen;
+            BtnSeConnecter.ButtonColorEnd = Color.DarkBlue;
+            BtnSeConnecter.ButtonColorStart = Color.RoyalBlue;
             BtnSeConnecter.ButtonImage = null;
             BtnSeConnecter.CanBeep = false;
             BtnSeConnecter.CanShake = false;
@@ -78,7 +81,7 @@
             BtnSeConnecter.ImageRightMargin = 8;
             BtnSeConnecter.ImageSize = 24;
             BtnSeConnecter.IsReadOnly = false;
-            BtnSeConnecter.Location = new Point(247, 378);
+            BtnSeConnecter.Location = new Point(288, 384);
             BtnSeConnecter.MakeRadial = false;
             BtnSeConnecter.Name = "BtnSeConnecter";
             BtnSeConnecter.PressAnimationSpeed = 0.2F;
@@ -105,23 +108,24 @@
             // 
             // txtIdentifiant
             // 
-            txtIdentifiant.Location = new Point(201, 284);
+            txtIdentifiant.Location = new Point(216, 287);
             txtIdentifiant.Name = "txtIdentifiant";
-            txtIdentifiant.Size = new Size(191, 31);
+            txtIdentifiant.Size = new Size(210, 31);
             txtIdentifiant.TabIndex = 1;
             // 
             // txtMdp
             // 
-            txtMdp.Location = new Point(201, 333);
+            txtMdp.Location = new Point(216, 336);
             txtMdp.Name = "txtMdp";
-            txtMdp.Size = new Size(191, 31);
+            txtMdp.Size = new Size(210, 31);
             txtMdp.TabIndex = 2;
+            txtMdp.UseSystemPasswordChar = true;
             // 
             // lblidentifiant
             // 
             lblidentifiant.AutoSize = true;
             lblidentifiant.Font = new Font("Segoe UI Variable Text", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblidentifiant.Location = new Point(22, 288);
+            lblidentifiant.Location = new Point(25, 291);
             lblidentifiant.Name = "lblidentifiant";
             lblidentifiant.Size = new Size(129, 27);
             lblidentifiant.TabIndex = 3;
@@ -131,7 +135,7 @@
             // 
             lblmotdepasse.AutoSize = true;
             lblmotdepasse.Font = new Font("Segoe UI Variable Text", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblmotdepasse.Location = new Point(22, 337);
+            lblmotdepasse.Location = new Point(25, 340);
             lblmotdepasse.Name = "lblmotdepasse";
             lblmotdepasse.Size = new Size(149, 27);
             lblmotdepasse.TabIndex = 4;
@@ -142,7 +146,7 @@
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI Variable Text", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = Color.LightSlateGray;
-            label3.Location = new Point(22, 236);
+            label3.Location = new Point(25, 239);
             label3.Name = "label3";
             label3.Size = new Size(272, 27);
             label3.TabIndex = 5;
@@ -159,12 +163,23 @@
             fakebutton.UseVisualStyleBackColor = true;
             fakebutton.Click += FakeAcceptButton_Click;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(0, 43);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(448, 154);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
             // FrmAuthentification
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(424, 453);
+            ClientSize = new Size(449, 453);
+            Controls.Add(pictureBox1);
             Controls.Add(fakebutton);
             Controls.Add(label3);
             Controls.Add(lblmotdepasse);
@@ -172,8 +187,11 @@
             Controls.Add(txtMdp);
             Controls.Add(txtIdentifiant);
             Controls.Add(BtnSeConnecter);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "FrmAuthentification";
-            Text = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Supervisor+";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -187,5 +205,6 @@
         private Label lblmotdepasse;
         private Label label3;
         private Button fakebutton;
+        private PictureBox pictureBox1;
     }
 }
